372eb5f110cc026d4073d6259341fbff6476e5e0
givemin5 <givemin5@gmail.com>
2019-02-07 15:15:06 +0800

    Add project files.

---
+++ LeetCode961N_RepeatedElementInSize2NArray/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode961N_RepeatedElementInSize2NArray
+{
+    public class Solution
+    {
+        public int RepeatedNTimes(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LeetCode961N_RepeatedElementInSize2NArray/SourceTemplates.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace LeetCode961N_RepeatedElementInSize2NArray
+{
+    public static class SourceTemplates
+    {
+        [SourceTemplate]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ae<T>(this T source)
+        {
+            /*$ Assert.AreEqual( $expected$ , source);*/
+        }
+
+        [SourceTemplate]
+        public static void abs<T>(this T source)
+        {
+            /*$ Math.Abs(source)*/
+        }
+    }
+}

d595761191e692cfda6fcae6422dbed23c565653
givemin5 <givemin5@gmail.com>
2019-02-07 15:18:08 +0800

    Add First Test

--- LeetCode961N_RepeatedElementInSize2NArray/UnitTest1.cs
+++ LeetCode961N_RepeatedElementInSize2NArray/UnitTest1.cs
@@ -9,6 +9,16 @@ namespace LeetCode961N_RepeatedElementInSize2NArray
         [TestMethod]
         public void TestMethod1()
         {
+            var items = new int[] {1, 2, 3, 3};
+
+            Solution solution = new Solution();
+
+            var expected = 3;
+            var actual = solution.RepeatedNTimes(items);
+
+            Assert.AreEqual(expected, actual);
+
+
         }
     }
 }
